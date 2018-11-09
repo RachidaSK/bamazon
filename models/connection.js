3e85fd61e65cb393b54cc0ce97b831353b1713a3
RachidaSK <rachida.koco@gmail.com>
2018-11-09 17:45:43 -0500

    Set up Jaws connection

---
+++ models/connection.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection ({
+        host: "localhost",
+        user: "root",
+        password:"root",
+        database: "bamazon"
+    });
+} 
+
+connection.connect();
+
+module.exports = connection;
